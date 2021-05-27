732f5b61fb5a5d639fd1bf8863c12a325eff35a0
“polrb5” <polreigbroto@gmail.com>
2021-05-27 13:46:29 +0200

    First commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const apellido1 = "Pérez";
+const apellido2 = "Cattani";
+let a = 'jhg'
+const esItaliano = (origen) => {
+  if ()
+}
+
+
+
+
+
+`${} podría ser de cualquier país`
+`${} tiene muchas probabilidades de ser italiano`
